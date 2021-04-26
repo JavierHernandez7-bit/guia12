30464a09bd44e566a356eac0bef7540e0f860ec1
JavierHernandez7-bit <hm210444@alumno.udb.edu.sv>
2021-04-25 23:06:53 -0600

    subiendo guia 12

---
+++ ejercicio1Guia12/ejercicio1Guia12/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ejercicio1Guia12
+{
+    class Program
+    {
+        struct programa
+        {
+            public String universidad;
+            public String carrera;
+        }
+        struct estudiante
+        {
+            public String nombre;
+            public int edad;
+            public programa estudios;
+        }
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejemplo3 de estructuras anidadas";
+            estudiante estudiante = new estudiante();
+            Console.WriteLine("Ingrese el nombre:");
+            estudiante.nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese la Edad: ");
+            estudiante.edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la carrera en estudio:");
+            estudiante.estudios.carrera = Console.ReadLine();
+            Console.WriteLine("Ingrese la Universidad");
+            estudiante.estudios.universidad = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("MOSTRANDO DATOS PROCESADOS....");
+            Console.WriteLine("Estudiante: " + estudiante.nombre);
+            Console.WriteLine("Edad: " + estudiante.edad);
+            Console.WriteLine("Carrera: " + estudiante.estudios.carrera);
+            Console.WriteLine("Universidad: " + estudiante.estudios.universidad);
+            Console.ReadKey();
+        }   
+    }
+}
---
+++ ejercicio3Guia12/ejercicio3Guia12/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace n_vehiculos_procedimiento
+{
+    struct Vehiculos
+    {
+
+        public int placa;
+        public string color;
+        public int ano;
+        public string marca;
+        public string modelo;
+        public string propietario;
+        static void Main(string[] args)
+        {
+            int cantidadMax;
+            Console.WriteLine("Ingrese la cantidad maxima/total de vehiculos automotores");
+            cantidadMax = int.Parse(Console.ReadLine());
+            Vehiculos[] carros = new Vehiculos[cantidadMax];
+            Console.WriteLine("\n");
+            for (int i = 0; i < cantidadMax; i++)
+            {
+                Console.WriteLine("Ingrese la *marca* del vehiculo automotor");
+                carros[i].marca = Console.ReadLine();
+                Console.WriteLine("Ingrese *modelo* del vehiculo automotor");
+                carros[i].modelo = Console.ReadLine();
+                Console.WriteLine("Ingrese el *color* del vehiculo automotor");
+                carros[i].color = Console.ReadLine();
+                Console.WriteLine("Ingrese el *año* del vehiculo automotor");
+                carros[i].ano = int.Parse(Console.ReadLine());
+                Console.WriteLine("NOTA°: Para el siguiente apartado, colocar unicamente los números, omitir letras y guiones...");
+                Console.WriteLine("Ingrese el número° de *placa* del vehiculo automotor");
+                carros[i].placa = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese nombre del propietario");
+                carros[i].propietario = Console.ReadLine();
+            }
+
+            Console.Clear();
+            Console.WriteLine("La informacion de su/sus vehiculos automotores se procesó de la siguiente manera: ");
+            Console.WriteLine("\n");
+            for (int i = 0; i < cantidadMax; i++)
+            {
+                Console.WriteLine("**************************");
+                Console.WriteLine("Marca: " + carros[i].marca);
+                Console.WriteLine("Modelo: " + carros[i].modelo);
+                Console.WriteLine("Color: " + carros[i].color);
+                Console.WriteLine("Año: " + carros[i].ano);
+                Console.WriteLine("Placa: " + carros[i].placa);
+                Console.WriteLine("Propietario: " + carros[i].propietario);
+                Console.WriteLine("**************************");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
